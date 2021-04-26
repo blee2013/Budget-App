92ab893e0fc98cdf308f5f07ccc3bbb23a642143
Brittany Moes <brittanylee@Brittanys-MacBook-Pro.local>
2021-04-25 21:07:19 -0500

    Deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,25 @@
 // service worker will cache the app data for offline use
-
-const FILES_TO_CACHE = [
-    "./index.html",
-]
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+   
+    "/index.html",
+    "/css/styles.css",
+    "/js/idb.js",
+    "/js/index.js",
+    "/manifest.json",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    // "../models/transaction.js"
+  
+
+]
+
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
@@ -37,10 +51,8 @@ self.addEventListener('activate', function (e) {
         }));
 });
 
-// tells browser how to handle fetch requests
 self.addEventListener('fetch', function (e) {
     console.log('fetch request: ' + e.request.url)
-    // intecept fetch request
     e.respondWith(
         caches.match(e.request).then(function (request) {
             if (request) {
